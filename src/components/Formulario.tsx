c7a7d71b2e4f3f7e462911af9b76f60ad1108451
Lilian1306 <kbts2207@gmail.com>
2025-11-05 22:56:23 -0600

    Iniciando el proyecto

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Formulario() {
+  return (
+    <div className="">
+        <h2 className="text-center mt-10">Mi nuevo Formulario</h2>
+        <p></p>
+    </div>
+  )
+}
