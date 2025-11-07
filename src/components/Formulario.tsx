595c29a6639e60717a2ae95cf3d2509cc0133092
Lilian1306 <kbts2207@gmail.com>
2025-11-06 18:43:31 -0600

    Agreando el select y agregando las opciones y estidos

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -5,43 +5,47 @@ export default function Formulario() {
     <div className="">
         <h2 className="text-center mt-20">Mi nuevo Formulario</h2>
 
-        <div className="items-center mt-10 flex flex-col">
-          <label htmlFor="name" className="mb-2 text-gray-700"
+        <div className="items-center mt-10 flex flex-col  bg-purple-200 shadow-xl rounded-lg p-10 w-96 mx-auto">
+        
+          <label htmlFor="name" className=" text-gray-700"
           >Escriba tu nombre: </label>
           <input
            type="text"
            id="name"
            placeholder="Escribe tu nombre aqui"
-           className="border border-gray-300 rounded-lg p-2 w-80"
+           className="border border-gray-500 rounded-lg p-2 w-full"
           />
 
-          <label htmlFor="email" className="text-gray-700"
+          <label htmlFor="email" className="text-gray-700 mt-5"
           >Escribe tu correo electronico:</label>
           <input
             type="email"
             id="email"
             placeholder="Escribe tu correo aqui"
-            className="border border-gray-300 rounded-lg p-2 w-80"
+            className="border border-gray-500 rounded-lg p-2 w-full"
           />
 
         <div>
         
           <select
              id="cards"
-             className="border border-gray-300 rounded-lg w-80 mt-5"
+             className="border border-gray-500 rounded-lg w-50 mt-5"
           >
-            <option>-- Selecciona una opcion --</option>
-            <option value="" ></option>
-            <option value="" ></option>
-            <option value="" ></option>
-            <option value="" ></option>
-            <option value="" ></option>
-            <option value="" ></option>
-            <option value="" ></option>
+            <option className="text-center"> Fan Chant!! </option>
+            <option value="" className="text-center" >Kim NamJoon</option>
+            <option value="" className="text-center"  >Kim SeokJin</option>
+            <option value="" className="text-center" >Min Yoongi</option>
+            <option value="" className="text-center" >Jung Hoseok</option>
+            <option value="" className="text-center" >Park Jimin</option>
+            <option value="" className="text-center" >Kim Taeyhung</option>
+            <option value="" className="text-center" >Jeon JungKook</option>
+            <option value="" className="text-center" >BTS</option>
           </select>
 
           </div>
+          
         </div>
+        
     </div>
   )
 }
