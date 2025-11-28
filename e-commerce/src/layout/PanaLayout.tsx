d0c22605bea6c4a7d6e0efff3bc39d87ce6460ce
Lilian1306 <kbts2207@gmail.com>
2025-11-28 16:53:15 -0600

    adding styles

--- e-commerce/src/layout/PanaLayout.tsx
+++ e-commerce/src/layout/PanaLayout.tsx
@@ -4,22 +4,21 @@ import { Outlet } from "react-router-dom";
 export default function PanaLayout() {
   return (
     <>
-        <header className="flex flex-row justify-between items-center h-30 px-30 border-gray-200 bg-sky-500 top-0 z-10">
-            <div className="flex flex-row gap-4 items-center text-white text-sm font-medium text-gray-900  cursor-pointer">
-                <span className="text-xl"></span>
-                <span>Guatemala</span>
-                <span className="text-white">|</span>
-                <span>English</span>
-            </div>   
+        <header className="flex flex-row justify-between items-center w-full px-4 md:px-10 bg-sky-500 shadow-md relative z-10 py-2 min-h-[80px]">
 
-            <div className="absolute left-1/2 transform -translate-x-1/2">
-              <h1 className="text-center text-2xl text-white">Panajachel</h1>
-            </div>   
+            <div className="flex flex-col items-start gap-1">
+                 <h1 className="text-2xl md:text-4xl font-bold text-white tracking-widest  drop-shadow-md static md:absolute md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 ">Panajachel</h1>
 
-            <img
-              src="/logo.avif"
-              className="h-20 w-30 rounded-xl shadow-4xl"
-            /> 
+                <span className="flex flex-row">Guatemala | English</span>
+        
+            </div>          
+            <div>
+              <img
+                src="/logo.avif"
+                className="h-28  w-auto rounded-xl shadow-2xl"
+             /> 
+            </div>
+          
         </header>
 
 
--- e-commerce/src/views/MainPage.tsx
+++ e-commerce/src/views/MainPage.tsx
@@ -2,6 +2,10 @@
 
 export default function MainPage() {
   return (
-    <div className="text-4xl"></div>
+    <div className="text-4xl mt-5 gap-2">
+      <img
+        src="/Panajachel.png"
+      />
+    </div>
   )
 }
