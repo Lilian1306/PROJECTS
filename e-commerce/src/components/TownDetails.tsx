e90430a3310f91273bd43a90c46bdc8dc498d5ff
DaniM0613 <wwh.rj.92@gmail.com>
2025-12-21 21:20:22 -0600

    last update

--- e-commerce/src/components/TownDetails.tsx
+++ e-commerce/src/components/TownDetails.tsx
@@ -13,18 +13,19 @@ export default function TownDetails() {
     <>
       <div className="max-w-4xl mx-auto p-4">
         <div>
-          <img src={townData.heroImage} alt={townData.name} className="w-full h-100"/>
+          <img 
+            src={townData.heroImage} 
+            alt={townData.name} 
+            className="w-full h-full object-cover"/>
+
+            <div className="absolute bottom-0 left-0 bg-liner-to-t from-black/70 p-6 w-full">
+              <h1 className="text-black texl-3xl font-bold">{townData.name}</h1>
+            </div>
         </div>
+
         
       </div>
-      <div>
-        <h2 className="text-xl font-bold mt-4">Actividades</h2>
-        <ul className="list-disc ml-5">
-          {townData.details.activities.map((activity, index) => (
-            <li key={index}>{activity}</li>
-          ))}
-        </ul>
-      </div>
+      
     </>
   )
 }
