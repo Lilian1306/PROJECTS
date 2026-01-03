2fa7ca1e8a8c867d64bc71e602df0947699d7b2c
DaniM0613 <wwh.rj.92@gmail.com>
2026-01-03 13:44:28 -0600

    Adding the styles in the header

--- e-commerce/src/layout/PanaLayout.tsx
+++ e-commerce/src/layout/PanaLayout.tsx
@@ -1,35 +1,35 @@
 import { Outlet } from "react-router-dom";
 import DarkMode from "../utils/DarkMode";
 
-
 export default function PanaLayout() {
   return (
     <>
-        <header className="flex flex-row justify-between items-center w-full px-4 md:px-10 bg-sky-500 shadow-md relative z-10 py-2 min-h-[80px]">
+      <header className="flex flex-row justify-between items-center w-full px-4 md:px-10 bg-sky-500 shadow-md relative z-10 py-4 min-h-[100px]">
+  
+        <div className="flex items-center gap-4">
+          <img
+            src="/logo.avif"
+            alt="Logo Atitlan"
+            className="h-20 md:h-24 w-auto rounded-xl shadow-2xl"
+          />
+          <h1 className="text-2xl md:text-4xl font-bold text-white tracking-widest drop-shadow-md">
+            Panajachel
+          </h1>
+        </div>
+        <div className="flex flex-col items-end gap-2 text-white font-medium">
+          <span className="text-sm md:text-base border-b border-sky-300 pb-1">
+            Guatemala 
+          </span>
+          <div className="mt-1">
+            <DarkMode />
+          </div>
+        </div>
 
-            <div className="flex flex-col items-start gap-1">
-                 <h1 className="text-2xl md:text-4xl font-bold text-white tracking-widest  drop-shadow-md static md:absolute md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 ">Panajachel</h1>
+      </header>
 
-                <span className="flex flex-row">Guatemala | English</span>
-        
-            </div>          
-            <div >
-              <img
-                src="/logo.avif"
-                alt="Logo Atitlan"
-                className="h-28  w-auto rounded-xl shadow-2xl"
-             /> 
-            </div>
-
-            <DarkMode/>
-          
-        </header>
-
-        <section>
-            <Outlet/>
-        </section>
+      <section>
+        <Outlet />
+      </section>
     </>
-  )
+  );
 }
-
-
