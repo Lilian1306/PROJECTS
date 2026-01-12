daf66a640c50cc0bf1ddd0de5419282296185229
Lilian1306 <kbts2207@gmail.com>
2026-01-12 09:03:46 -0600

    Creating the main page

--- Weather/src/Router.tsx
+++ Weather/src/Router.tsx
@@ -1,11 +1,14 @@
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainPage from "./views/MainPage";
 
 
 
 export default function router() {
   return (
-    <BrowserRouter      >
-      
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>}/>
+      </Routes>
     </BrowserRouter>
   )
 }
