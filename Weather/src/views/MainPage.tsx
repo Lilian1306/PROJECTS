99f44d2679f1221c0a57e1444851f484986bf7e8
Lilian1306 <kbts2207@gmail.com>
2026-02-09 09:08:29 -0600

    Adding the select tag

--- Weather/src/views/MainPage.tsx
+++ Weather/src/views/MainPage.tsx
@@ -5,7 +5,17 @@ export default function MainPage() {
   return (
     <>
       <div className="mt-30">
-        <form className="items-center flex flex-col bg-sky-400 shadow rounded-lg p-3 w-96 mx-auto ">What is the weather like today?</form>
+        <form className="items-center flex flex-col bg-sky-500 shadow rounded-lg p-3 w-96 mx-auto font-bold text-white "> 
+          <div className="mb-5">What is the weather like today? </div>
+
+        <select 
+            id="country"
+            className="border border-gray-200 rounded-lg w-60  mt-5 p-2 text-black">
+          <option>Selecciona tu pais: </option>
+        </select>
+        </form>
+
+     
       </div>
     </>
   )
